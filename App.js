c939b8b1bca743051b4915a5c50fe9fc988b5493
ravi <artiveloper@gmail.com>
2020-05-21 17:16:22 +0900

    feat: App Header를 추가한다

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    SafeAreaView,
+    StyleSheet,
+} from 'react-native';
+import Header from 'components/Header';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Header/>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
 });
